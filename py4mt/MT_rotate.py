7519a2d7bc9e33bdc0b7f6da2de5b2db2113741f
vrath <vrath@cp.dias.ie>
2020-01-02 16:39:27 +0100

    improved rotation script

--- py4mt/MT_rotate.py
+++ py4mt/MT_rotate.py
@@ -9,11 +9,17 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.3.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
 # ---
 
 """
+Rotate imedance tensor (Z) and tipper (T)  
+by potentially different angles into NS/EW coordinate system
 
-@author: sb & vr oct 2019
+@author: sb & vr Jan 2020
 
 """
 
@@ -29,11 +35,26 @@ import numpy as np
 edi_in_dir = 'edifiles_test/'
 print(' Edifiles read from: %s' % edi_in_dir)
 
-# Define the path anf appended string for saved EDI-files
+# Define the path anf appended string for saved EDI-files:
 
 edi_out_dir= 'edifiles_test/'
 print(' Edifiles written from: %s' % edi_out_dir)
 out_string = '_rot0'
+
+# This example brings both angles to the normal (measurement) coordinate 
+# system, i. e., afterwards the ZROT and TROT fields in tehe EDI-files are 
+# both zero. Here Z_angle and T_angle are the rotation angles from the 
+# input EDI-file. 
+
+
+Z_rot=('-1.*Z_angle')
+T_rot=('-1.*T_angle')
+
+# As the actual call is done using the 'eval' function, this script can 
+# also be used to do rotations of different kind, e.g. when using a rotated 
+# coordinate system for modelling. In this case to rotate from the zero system 
+# to a sytem rotated 40 degrees clockwise, the string would look like 
+# '40.*np.ones(shape(Z_angle))'.
 
 
 # No changes required after this line!
@@ -42,7 +63,7 @@ out_string = '_rot0'
 
 
 edi_files=[]
-files= os.listdir(edi_in_dir) 
+files= os.listdir(edi_in_dir) # input EDI-file. As the actual call is dine using the 'eval' function, 
 for entry in files:
    # print(entry)
    if entry.endswith('.edi') and not entry.startswith('.'):
@@ -54,30 +75,36 @@ ns =  np.size(edi_files)
 for filename in edi_files :
     print('\n Reading data from '+edi_in_dir+filename)
     name, ext = os.path.splitext(filename)
-    
+
 # Create an MT object 
 
     file_in = edi_in_dir+filename
     mt_obj = MT(file_in)
     print(' site %s at :  % 10.6f % 10.6f' % (name, mt_obj.lat, mt_obj.lon))
-    rot_angle=-1.*mt_obj.Z.rotation_angle
-    print(mt_obj.Tipper.rotation_angle)
-    mt_obj.Z.rotate(rot_angle)
-    mt_obj.Tipper.rotate(-1.*rot_angle)
-    print(rot_angle)
     
+    Z_angle=mt_obj.Z.rotation_angle
+    T_angle=mt_obj.Tipper.rotation_angle
+    
+    Z_rot_angle = eval(Z_rot)
+    T_rot_angle=  eval(T_rot)
+
+
+    mt_obj.Z.rotate(Z_rot_angle)
+    mt_obj.Tipper.rotate(T_rot_angle)
+
+
 # Write a new edi file:
-    
-    file_out=filename+out_string+ext
+
+    file_out=name+out_string+ext
     print('Writing data to '+edi_out_dir+file_out)
     mt_obj.write_mt_file(
             save_dir=edi_out_dir,
             fn_basename=file_out,
             file_type='edi',
-            new_Z_obj=mt_obj.Z, # provide a z object to update the data
-            longitude_format='LONG', # write longitudes as 'LONG' not ‘LON’
-            latlon_format='dd'# write as decimal degrees (any other input
-            # will write as degrees:minutes:seconds
+            new_Z_obj=mt_obj.Z, 
+            new_Tipper_obj=mt_obj.Tipper,
+            longitude_format='LONG',
+            latlon_format='dd'
             )
 
   
