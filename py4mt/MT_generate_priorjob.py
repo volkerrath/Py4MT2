7c87bdbb94fd21f73cf43f2ddc9f2a8313e6a711
Volker Rath <vrath@cp.dias.ie>
2021-03-05 19:42:35 +0100

    adapted to new conventions

--- py4mt/MT_generate_priorjob.py
+++ py4mt/MT_generate_priorjob.py
@@ -18,10 +18,33 @@ Created on Fri Sep 11 15:41:25 2020
 @author: vrath
 """
 # Import required modules
+
 import os
+import sys
+from sys import exit as error
+import time
+from datetime import datetime
+import warnings
+
 import numpy as np
-from modules.util import strcount, strdelete, strreplace, unique
-from modules.modem import readDat
+
+mypath = ["/home/vrath/Py4MT/py4mt/modules/",
+          "/home/vrath/Py4MT/py4mt/scripts/"]
+for pth in mypath:
+    if pth not in sys.path:
+        sys.path.insert(0,pth)
+# Import required modules
+
+import util as utl
+import modem as mod
+from version import versionstrg
+
+Strng, _ = versionstrg()
+now = datetime.now()
+print("\n\n"+Strng)
+print("Generate prior test job"+"\n"+"".join("Date " + now.strftime("%m/%d/%Y, %H:%M:%S")))
+print("\n\n")
+
 
 
 Data_tmp = '.TMP'
@@ -29,21 +52,21 @@ Data_tmp = '.TMP'
 Name = 'Maur_PT_R500'
 Exe_in = '/home/vrath/work/MaurienneJCN/RunGnuFrog.sh'
 Exe_out = '/home/vrath/work/MaurienneJCN/RunGnuFrog_' + Name + '.sh'
-strreplace('RUNNAME', Name, Exe_in, Exe_out)
+utl.strreplace('RUNNAME', Name, Exe_in, Exe_out)
 
 Prior_in = '/home/vrath/work/MaurienneJCN/MaurPrior.rho'
 Prior_out = '/home/vrath/work/MaurienneJCN/MaurPrior_R500.rho'
 prior_val = 5.00000E+2
-strreplace('PRIOR-VAL', str(prior_val), Prior_in, Prior_out)
+utl.strreplace('PRIOR-VAL', str(prior_val), Prior_in, Prior_out)
 
 Covar_in = '/home/vrath/work/MaurienneJCN/Maur.cov'
 Covar_out = '/home/vrath/work/MaurienneJCN/Maur_02.cov'
 a_x = 0.2
 a_y = 0.2
 a_z = 0.2
-strreplace('COVX', str(a_x), Covar_in, 'tmp1')
-strreplace('COVY', str(a_y), 'tmp1', 'tmp2')
-strreplace('COVZ', str(a_z), 'tmp2', Covar_out)
+utl.strreplace('COVX', str(a_x), Covar_in, 'tmp1')
+utl.strreplace('COVY', str(a_y), 'tmp1', 'tmp2')
+utl.strreplace('COVZ', str(a_z), 'tmp2', Covar_out)
 
 DataFiles = ['/home/vrath/work/MaurienneJCN/Maur_PT.dat']
 # '/home/vrath/work/MaurienneJCN/Maur_T.dat',
@@ -56,10 +79,10 @@ RunNamebase = Name
 
 
 for f in DataFiles:
-    Site, Comp, Data, Head = readDat(f)
-    Freqs = unique(Data[:, 0])
+    Site, Comp, Data, Head = mod.read_data(f)
+    Freqs = utl.unique(Data[:, 0])
     NFreq = np.size(Freqs)
-    Sites = unique(Site)
+    Sites = utl.unique(Site)
     NSits = np.size(Sites)
     sData = np.shape(Data)
     NdTot = sData[0]
@@ -67,7 +90,7 @@ for f in DataFiles:
     OldHead = Head[7]
 
     for place in Sites:
-        NdSite = strcount(place, f)
+        NdSite = utl.strcount(place, f)
         NdJCN = NdTot - NdSite
         print('Number of data from site ' + place + ' is ' + str(NdSite))
         print('Number of JCN sample is ' + str(NdJCN))
@@ -75,18 +98,18 @@ for f in DataFiles:
         name, ext = os.path.splitext(f)
         Data_out = name + '_No' + place + ext
         print('New datafile: ' + Data_out)
-        strdelete(place, f, Data_tmp, out=True)
+        utl.strdelete(place, f, Data_tmp, out=True)
 
-        _, _, D, _ = readDat(Data_tmp)
-        Freqs_out = unique(D[:, 0])
+        _, _, D, _ = mod.read_data(Data_tmp)
+        Freqs_out = utl.unique(D[:, 0])
         NFreq_out = np.size(Freqs_out)
         Headstr = '> ' + str(NFreq_out) + '   ' + str(NSits - 1) + '  \n'
-        strreplace(OldHead, Headstr, Data_tmp, Data_out)
+        utl.strreplace(OldHead, Headstr, Data_tmp, Data_out)
 
         name, ext = os.path.splitext(InvFilebase)
         InvFile = name + '_No' + place + ext
         RunName = RunNamebase + '_No' + place
-        strreplace('RUNNAME', RunName, InvFilebase, InvFile)
+        utl.strreplace('RUNNAME', RunName, InvFilebase, InvFile)
 
         filep = Prior_out.split(os.sep)[-1]
         filed = Data_out.split(os.sep)[-1]
