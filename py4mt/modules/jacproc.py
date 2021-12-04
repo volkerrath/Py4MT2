error in modem jacobian read corrected, sparse matrix merge added

py4mt/modules/jacproc.py
@@ -163,7 +163,7 @@ def ortho_basis(M):
     return Q
 
 
-def sparsify_jac(Jac=None, sparse_thresh=1.0e-6, normalized=True, method=None, out=True):
+def sparsify_jac(Jac=None, sparse_thresh=1.0e-6, normalized=False, method=None, out=True):
     """
     Sparsifies error_scaled Jacobian from ModEM output
 
@@ -177,27 +177,26 @@ def sparsify_jac(Jac=None, sparse_thresh=1.0e-6, normalized=True, method=None, o
             "sparsifyJac: dimension of original J is %i x %i = %i elements"
             % (shj[0], shj[1], nel)
         )
-
-    Jac = np.abs(Jac)
-    Jmax = np.amax(Jac)
+    Jf = np.abs(Jac.copy())
+    Jmax = np.amax(Jf)
     thresh = Jmax * sparse_thresh
-    Jac[Jac < thresh] = 0.0
-    Js = scp.csr_matrix(Jac)
+    Jf[Jf < thresh] = 0.0
+    Js = scp.csr_matrix(Jf)
 
     if out:
         if scp.issparse(Js):
             ns = scp.csr_matrix.count_nonzero(Js)
             print(
                 "sparsifyJac:"
-                +" output J is sparse: %r, and has %i nonzeros, %f percent"
-                % (scp.issparse(Js), ns, 100.0 * ns / nel)
+                +" output J is sparse, and has %i nonzeros, %f percent"
+                % (ns, 100.0 * ns / nel)
             )
         x = np.random.normal(size=np.shape(Jac)[1])
         # print(np.shape(x))
         # print(np.shape(Js))
         normx = npl.norm(x)
         norma = npl.norm(Js@x)/normx
-        normf = npl.norm(Jac@x)/normx
+        normf = npl.norm(Jf@x)/normx
         # print(norma)
         # print(normf)
         print(" Sparsified J explains "
@@ -231,7 +230,7 @@ def normalize_jac(Jac=None, fn=None, out=True):
 
 def calculate_sens(Jac=None, normalize=True, small=1.0e-14, log=False, out=True):
     """
-    Calculate sensitivity Jacobian from ModEM output.
+    Calculate sensitivity from ModEM Jacobian.
 
     author: vrath
     last changed: Sep 25, 2020
@@ -242,18 +241,18 @@ def calculate_sens(Jac=None, normalize=True, small=1.0e-14, log=False, out=True)
         J = Jac
 
     S = np.sum(np.power(J, 2), axis=0)
-    S=np.sqrt(S)
+    S = np.sqrt(S)
 
     Smax = np.amax(S)
+
     if normalize:
         S = S / Smax
 
+    if small >= 1.0e-14:
+        S[S < small] = 1.0e-20
+
     if log:
         S = np.log10(S)
-
-    if small >= 1.0e-14:
-        S[S < small] = np.NaN
-
 
     return S, Smax
 

py4mt/modules/modem.py
@@ -48,7 +48,7 @@ def read_jac(JacFile=None, out=False):
                 # header2
                 header2 = fjac.read_record(np.byte)
                 h2 = ''.join([chr(item) for item in header2])
-                # print(h2)
+                print(h2)
                 # print(i1,i2,i3)
                 nSigma = fjac.read_ints(np.int32)
                 # print("nSigma"+str(nSigma))
@@ -68,7 +68,8 @@ def read_jac(JacFile=None, out=False):
                     _ = fjac.read_reals(np.float64)
                     # AirCond
                     _ = fjac.read_reals(np.float64)
-                    ColJac = fjac.read_reals(np.float64).flatten(order="F")
+                    # ColJac = fjac.read_reals(np.float64).flatten(order="F")
+                    ColJac = fjac.read_reals(np.float64).flatten()
                     # print(np.shape(CellSens))
                     # ColJac =  CellSens.flatten(order='F')
                     tmp1.append(ColJac)

py4mt/scripts/MT_proc_jac.py
@@ -75,26 +75,35 @@ sparse_thresh = 1.e-7
 WorkDir = r"/home/vrath/work/MT_Data/Ubaye/UB22_jac_best/"
 MFile   = WorkDir +r"Ub22_ZoffPT_02_NLCG_014.rho"
 
-JFile = [WorkDir+r"Ub22_Zoff.jac", ]
-DFile = [WorkDir+r"Ub22_Zoff.dat", ]
+# JFile = [WorkDir+r"Ub22_Zoff.jac", ]
+# DFile = [WorkDir+r"Ub22_Zoff.dat", ]
 
 # JFile = [WorkDir+r"Ub22_P.jac", ]
 # DFile = [WorkDir+r"Ub22_P.dat", ]
 
 # JFile = [WorkDir+r"Ub22_T.jac", ]
-# DFile = [WorkDir+r"Ub22_T.dat",]
+# DFile = [WorkDir+r"Ub22_T.dat", ]
+
+JFile = [WorkDir+r"Ub22_Zoff.jac", WorkDir+r"Ub22_P.jac", WorkDir+r"Ub22_T.jac", ]
+DFile = [WorkDir+r"Ub22_Zoff.dat", WorkDir+r"Ub22_P.dat", WorkDir+r"Ub22_T.dat", ]
 
 
 total = 0.0
 
 
 start = time.time()
-dx, dy, dz, rho, reference = mod.read_model(MFile, trans="log10")
+dx, dy, dz, rho, reference = mod.read_model(MFile, trans="linear")
 dims = np.shape(rho)
-# print(dims)
+print(dims)
+print(reference)
 elapsed = time.time() - start
 total = total + elapsed
 print(" Used %7.4f s for reading model from %s " % (elapsed, MFile))
+# rr = rho.copy()
+# SNSFile = os.path.split(MFile)[0]+"_test.rho"
+# np.shape(rr)
+# mod.write_model(SNSFile, dx, dy, dz, rr, reference, trans="linear")
+# dx, dy, dz, rho2, reference = mod.read_model(SNSFile, trans="linear")
 
 
 if np.size(DFile) != np.size(JFile):
@@ -160,18 +169,22 @@ for f in np.arange(nF):
         elapsed = time.time() - start
         total = total + elapsed
         print(" Used %7.4f s for writing sparsified Jacobian to %s " % (elapsed, NPZFile))
+        if f==0:
+            JacStack = Jacs.copy()
+        else:
+            JacStack = scs.vstack([JacStack, Jacs.copy()])
 
 
     start = time.time()
     S, Smax = jac.calculate_sens(Jac, normalize=False, small=1.0e-14)
-    S = np.reshape(S, dims, order="F")
     elapsed = time.time() - start
     total = total + elapsed
     print(" Used %7.4f s for calculating Sensitivity from Jacobian  %s " % (elapsed, JFile[f]))
 
     start = time.time()
     SNSFile = name+nstr+".sns"
-    mod.write_model(SNSFile, dx, dy, dz, S, reference, trans="log10")
+    S = np.reshape(S, dims, order="F")
+    mod.write_model(SNSFile, dx, dy, dz, S, reference, trans="linear")
     elapsed = time.time() - start
     total = total + elapsed
     print(" Used %7.4f s for writing Sensitivity from Jacobian  %s " % (elapsed, JFile[f]))
@@ -183,4 +196,13 @@ for f in np.arange(nF):
     total = total + elapsed
     print(" Used %7.4f s for writing Jacobian to %s " % (elapsed, NCFile))
 
-print("\n\nUsed %7.4f s for processing Jacobian  %s " % (total))
+
+NPZFile = name+nstr+sstr+"_JacStack_jacs.npz"
+scs.save_npz(NPZFile, Jacs)
+NPZFile = name+nstr+sstr+"_JacStack_dats.npz"
+np.savez(NPZFile, Data=Data, Site=Site, Comp=Comp)
+elapsed = time.time() - start
+total = total + elapsed
+print(" Used %7.4f s for writing sparsified Jacobian to %s " % (elapsed, NPZFile))
+
+print("\n\nUsed %7.4f s for processing Jacobian." % (total))